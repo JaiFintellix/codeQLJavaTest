468a116acf819c59bb6113a3b1d65c5a1b28aa8d
Jai Aggarwal <jai.aggarwal@fintellix.com>
2024-05-08 20:38:49 +0530

    random change 3

--- src/main/java/org/example/VulnerableCode.java
+++ src/main/java/org/example/VulnerableCode.java
@@ -62,7 +62,7 @@ public class VulnerableCode {
     private static final String API_KEY = "YOUR_API_KEY";
 
     public String getApiKey() {
-        System.out.println("Random Text Change1");
+        System.out.println("Random Text Change2");
         return API_KEY;
     }
 }
