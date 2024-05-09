caea80a71166f5fce06bcc922c178f80fe2d86fd
Jai Aggarwal <jai.aggarwal@fintellix.com>
2024-05-09 10:17:53 +0530

    Vulnerable Code 2

---
+++ src/main/java/org/example/VulnerableCode2.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.io.*;
+
+public class VulnerableCode2 {
+    public static void main(String[] args) {
+        // SQL Injection Vulnerability
+        String userInput = args[0];
+        String sqlQuery = "SELECT * FROM users WHERE username = '" + userInput + "'";
+
+        // Command Injection Vulnerability
+        try {
+            String command = "ls " + userInput;
+            Process p = Runtime.getRuntime().exec(command);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String s;
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Cross-Site Scripting (XSS) Vulnerability
+        String userMessage = "<script>alert('You have been hacked!');</script>";
+        System.out.println("User message: " + userMessage);
+
+        // Insecure Random Number Generator Vulnerability
+        double randomValue = Math.random();
+        System.out.println("Random value: " + randomValue);
+    }
+}
